cad4e7fe7579e28aaed036c680f178f01e550ec1
Ross Jacobs <rj@swit.sh>
2020-02-17 00:46:40 +0000

    Added parsing of auth json

--- fastlogin/fastlogin.go
+++ fastlogin/fastlogin.go
@@ -1,30 +1,47 @@
 package fastlogin
 
 import (
-	"fmt"
-	"io/ioutil"
-	"net/http"
-	"github.com/pocc/vpssites/logger"
+    "fmt"
+    "io/ioutil"
+    "net/http"
+    "github.com/pocc/vpssites/logger"
+    "encoding/json"
 )
+
+type Custdata struct {
+    Email string      `json:"email"`
+    Hash string       `json:"hash"`
+    Requester string  `json:"requester"`
+}
 
 // Show fastlog.in
 func HomePage(w http.ResponseWriter, r *http.Request) {
-	html, fileErr := ioutil.ReadFile("./fastlogin/index.html")
-	if fileErr != nil {
-	    logger.Error.Println("Unable to open local html file, ./fastlogin/index.html", fileErr.Error())
-	}
-	fmt.Fprintf(w, string(html))
+    html, fileErr := ioutil.ReadFile("./fastlogin/index.html")
+    if fileErr != nil {
+        logger.Error.Println("Unable to open local html file, ./fastlogin/index.html", fileErr.Error())
+    }
+    fmt.Fprintf(w, string(html))
 }
 
 // Designed for stripe's API to use this as a webhook
 // Also designed to save username/password from users
 func Auth(w http.ResponseWriter, r *http.Request) {
-	//retVal := fmt.Sprintf("%+v", r.Body)
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(string(body))
-	//Info.Println(retVal)
-	//w.Write([]byte(retVal))
+    var custrequest Custdata
+
+    body, readErr := ioutil.ReadAll(r.Body)
+    if readErr != nil {
+	httpFail(w, readErr)
+        return
+    }
+    jsonErr := json.Unmarshal(body, &custrequest)
+    if jsonErr != nil {
+	httpFail(w, jsonErr)
+        return
+    }
+    jsonData, _ := json.Marshal(custrequest)
+    w.Write(jsonData)
+}
+
+func httpFail(w http.ResponseWriter, err error) {
+    http.Error(w, err.Error(), http.StatusBadRequest)
 }
